ec72bc61bae9cd891e8e8594305ea26d8b906a17
Aman Raut <101492021+BadassAman4014@users.noreply.github.com>
2023-06-28 09:31:05 +0530

    Update HeapSort.c

--- HeapSort.c
+++ HeapSort.c
@@ -1,33 +1,84 @@
 #include <stdio.h>
-void swap(int* a, int* b) { int t = *a; *a = *b; *b = t; }
-void heapify(int arr[], int n, int i) {
-    int l = 2 * i + 1, r = 2 * i + 2, largest = i;
-    if (l < n && arr[l] > arr[largest]) largest = l;
-    if (r < n && arr[r] > arr[largest]) largest = r;
-    if (largest != i) swap(&arr[i], &arr[largest]), heapify(arr, n, largest);
+
+// Function to swap two elements
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
-void heapSortAsc(int arr[], int n) {
-    for (int i = n/2 - 1; i >= 0; i--) heapify(arr, n, i);
-    for (int i = n-1; i >= 0; i--) swap(&arr[0], &arr[i]), heapify(arr, i, 0);
+
+// Function to heapify a subtree rooted at index 'root'
+void heapify(int arr[], int size, int root) {
+    int largest = root; // Initialize largest as root
+    int left = 2 * root + 1; // Left child
+    int right = 2 * root + 2; // Right child
+
+    // If left child is larger than root
+    if (left < size && arr[left] > arr[largest])
+        largest = left;
+
+    // If right child is larger than largest so far
+    if (right < size && arr[right] > arr[largest])
+        largest = right;
+
+    // If largest is not root
+    if (largest != root) {
+        swap(&arr[root], &arr[largest]);
+        // Recursively heapify the affected sub-tree
+        heapify(arr, size, largest);
+    }
 }
-void heapSortDesc(int arr[], int n) {
-    for (int i = n/2 - 1; i >= 0; i--) heapify(arr, n, i);
-    for (int i = 0; i < n-1; i++) swap(&arr[0], &arr[n-i-1]), heapify(arr, n-i-1, 0);
+
+// Function to perform heap sort
+void heapSort(int arr[], int size, int order) {
+    // Build heap (rearrange array)
+    for (int i = size / 2 - 1; i >= 0; i--)
+        heapify(arr, size, i);
+
+    // One by one extract an element from the heap
+    for (int i = size - 1; i > 0; i--) {
+        // Move current root to the end
+        swap(&arr[0], &arr[i]);
+
+        // Heapify the reduced heap
+        heapify(arr, i, 0);
+    }
+
+    // Reverse the array if order is descending
+    if (order == -1) {
+        int start = 0;
+        int end = size - 1;
+        while (start < end) {
+            swap(&arr[start], &arr[end]);
+            start++;
+            end--;
+        }
+    }
 }
+
+// Function to print an array
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
 int main() {
-    int arr[] = {12, 11, 13, 5, 6, 7};
-    int n = sizeof(arr)/sizeof(arr[0]);
- 
+    int arr[] = { 12, 11, 13, 5, 6, 7 };
+    int size = sizeof(arr) / sizeof(arr[0]);
+
     printf("Original array: ");
-    for (int i = 0; i < n; ++i) printf("%d ", arr[i]); printf("\n");
+    printArray(arr, size);
 
-    heapSortAsc(arr, n);
-    printf("Ascending order sorted array: ");
-    for (int i = 0; i < n; ++i) printf("%d ", arr[i]); printf("\n");
+    // Ascending order heap sort
+    printf("Ascending order: ");
+    heapSort(arr, size, 1);
+    printArray(arr, size);
 
-    heapSortDesc(arr, n);
-    printf("Descending order sorted array: ");
-    for (int i = 0; i < n; ++i) printf("%d ", arr[i]); printf("\n");
+    // Descending order heap sort
+    printf("Descending order: ");
+    heapSort(arr, size, -1);
+    printArray(arr, size);
 
     return 0;
 }
